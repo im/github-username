74957d2a5041a9c64a15ec6d928a3d519ae0a640
糖小米 <tangxiaomiemail@gmail.com>
2019-08-21 08:03:01 +0800

    remove dist

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/github-username/dist/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/github-username/' : '/'
 }
